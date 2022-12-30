45a8fd6d5e096bbc2198a0ee2d99eb30d11c5458
fahim-muntasir <fahimmuntasir0909@gmail.com>
2022-12-31 01:09:10 +0600

    create single product get route

--- controller/product.js
+++ controller/product.js
@@ -3,139 +3,163 @@ const uniqId = require("uniqid");
 const fs = require("fs");
 
 const addProduct = async (req, res) => {
-  const { title, description, price, category } = req.body;
-  const productId = uniqId().toUpperCase();
+    const { title, description, price, category } = req.body;
+    const productId = uniqId().toUpperCase();
 
-  if (req.files.length > 0) {
-    try {
-      const createProduct = new Product({
-        productId,
-        title,
-        description,
-        price,
-        category,
-        avatar: req.files[0].filename,
-        creator: req.data.id,
-      });
-      await createProduct.save();
+    if (req.files.length > 0) {
+        try {
+            const createProduct = new Product({
+                productId,
+                title,
+                description,
+                price,
+                category,
+                avatar: req.files[0].filename,
+                creator: req.data.id,
+            });
+            await createProduct.save();
 
-      res.status(200).json({ msg: "Product create successfull." });
-    } catch {
-      res
-        .status(500)
-        .json({ errors: { msg: "Something is wrong on user create!" } });
+            res.status(200).json({ msg: "Product create successfull." });
+        } catch {
+            res.status(500).json({
+                errors: { msg: "Something is wrong on user create!" },
+            });
+        }
+    } else {
+        res.status(500).json({
+            errors: { avatar: "Please upload a product image!" },
+        });
     }
-  } else {
-    res
-      .status(500)
-      .json({ errors: { avatar: "Please upload a product image!" } });
-  }
+};
+
+// GET SINGLE PRODUCT
+const getProduct = async (req, res) => {
+    const { productId } = req.params;
+    try {
+        const products = await Product.findById(productId)
+            .populate("creator", "firstName lastName email role avatar")
+            .select({
+                __v: 0,
+            });
+        res.status(200).json(products);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ errors: { msg: "Product not found!" } });
+    }
 };
 
 // GET PRODUCTS
 const getProducts = async (req, res) => {
-  try {
-    const products = await Product.find()
-      .populate("creator", "firstName lastName email role avatar")
-      .select({
-        __v: 0,
-      });
-    res.status(200).json(products);
-  } catch (e) {
-    console.log(e);
-    res.status(500).json({ errors: { msg: "Product not found!" } });
-  }
+    try {
+        const products = await Product.find()
+            .populate("creator", "firstName lastName email role avatar")
+            .select({
+                __v: 0,
+            });
+        res.status(200).json(products);
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({ errors: { msg: "Product not found!" } });
+    }
 };
 
 // GET PRODUCTS BY STATUS
 const getProductsByStatus = async (req, res) => {
-  const { status } = req.params;
-  try {
-    const users = await Product.find({ status: status })
-      .populate("creator", "firstName lastName email role avatar")
-      .select({
-        __v: 0,
-      });
-    res.status(200).json(users);
-  } catch {
-    res.status(500).json({ errors: { msg: "Product not found!" } });
-  }
+    const { status } = req.params;
+    try {
+        const users = await Product.find({ status: status })
+            .populate("creator", "firstName lastName email role avatar")
+            .select({
+                __v: 0,
+            });
+        res.status(200).json(users);
+    } catch {
+        res.status(500).json({ errors: { msg: "Product not found!" } });
+    }
 };
 
 // UPDATE PRODUCT
 const updateProduct = async (req, res) => {
-  const productId = req.params.id;
+    const productId = req.params.id;
 
-  if (req.body?.status) {
-    if (req.data.role === "admin") {
-      try {
-        const updateProduct = await Product.updateOne(
-          { _id: productId },
-          { $set: { ...req.body } }
-        );
+    if (req.body?.status) {
+        if (req.data.role === "admin") {
+            try {
+                const updateProduct = await Product.updateOne(
+                    { _id: productId },
+                    { $set: { ...req.body } }
+                );
 
-        res.status(200).json({ success: true });
-      } catch {
-        res.status(500).json({
-          errors: { msg: "Somthing is wrong Product is not updated!" },
-        });
-      }
+                res.status(200).json({ success: true });
+            } catch {
+                res.status(500).json({
+                    errors: {
+                        msg: "Somthing is wrong Product is not updated!",
+                    },
+                });
+            }
+        } else {
+            res.status(403).json({
+                errors: { msg: "You are not allow for edit this!" },
+            });
+        }
     } else {
-      res
-        .status(403)
-        .json({ errors: { msg: "You are not allow for edit this!" } });
-    }
-  } else {
-    if (req.data.role === "admin" || req.data.role === "worker") {
-      try {
-        const updateProduct = await Product.updateOne(
-          { _id: productId },
-          { $set: { ...req.body } }
-        );
+        if (req.data.role === "admin" || req.data.role === "worker") {
+            try {
+                const updateProduct = await Product.updateOne(
+                    { _id: productId },
+                    { $set: { ...req.body } }
+                );
 
-        res.status(200).json({ success: true });
-      } catch {
-        res.status(500).json({
-          errors: { msg: "Somthing is wrong Product is not updated!" },
-        });
-      }
-    } else {
-      res
-        .status(403)
-        .json({ errors: { msg: "You are not allow for edit this!" } });
+                res.status(200).json({ success: true });
+            } catch {
+                res.status(500).json({
+                    errors: {
+                        msg: "Somthing is wrong Product is not updated!",
+                    },
+                });
+            }
+        } else {
+            res.status(403).json({
+                errors: { msg: "You are not allow for edit this!" },
+            });
+        }
     }
-  }
 };
 
 const updateProductImg = async (req, res) => {
-  const productId = req.params.id;
-  console.log(productId);
-  if (req.files.length > 0) {
-    fs.unlinkSync(
-      `${__dirname}/../public/uploads/productImages/${req.body.old_avatar}`
-    );
+    const productId = req.params.id;
+    console.log(productId);
+    if (req.files.length > 0) {
+        fs.unlinkSync(
+            `${__dirname}/../public/uploads/productImages/${req.body.old_avatar}`
+        );
 
-    try {
-      const updateProductImg = await Product.updateOne(
-        { _id: productId },
-        { $set: { avatar: req.files[0].filename } }
-      );
+        try {
+            const updateProductImg = await Product.updateOne(
+                { _id: productId },
+                { $set: { avatar: req.files[0].filename } }
+            );
 
-      res
-        .status(200)
-        .json({ success: true, updated_avatar: req.files[0].filename });
-    } catch {
-      res.status(500).json({
-        errors: { msg: "Somthing is wrong Product Img is not updated!" },
-      });
+            res.status(200).json({
+                success: true,
+                updated_avatar: req.files[0].filename,
+            });
+        } catch {
+            res.status(500).json({
+                errors: {
+                    msg: "Somthing is wrong Product Img is not updated!",
+                },
+            });
+        }
     }
-  }
 };
 
 module.exports = {
-  addProduct,
-  getProducts,
-  getProductsByStatus,
-  updateProduct,
-  updateProductImg,
+    addProduct,
+    getProducts,
+    getProductsByStatus,
+    updateProduct,
+    updateProductImg,
+    getProduct,
 };
--- routes/routes.js
+++ routes/routes.js
@@ -5,32 +5,38 @@ const { addUser, getUsers, getUsersByRole } = require("../controller/user");
 const { loginController, logoutController } = require("../controller/login");
 const checkLoginController = require("../controller/checkLogin");
 const {
-  addProduct,
-  getProducts,
-  getProductsByStatus,
-  updateProduct,
-  updateProductImg,
+    addProduct,
+    getProducts,
+    getProduct,
+    getProductsByStatus,
+    updateProduct,
+    updateProductImg,
 } = require("../controller/product");
 
 // IMPORT ALL MIDDLEWARE
 const {
-  userValidation,
-  userValidationResult,
+    userValidation,
+    userValidationResult,
 } = require("../middleware/validation/uservalidation");
 const userAvatarUpload = require("../middleware/avatarUploader/userAvatar");
 const {
-  loginUserDataValidation,
-  loginUserDataValidationResult,
+    loginUserDataValidation,
+    loginUserDataValidationResult,
 } = require("../middleware/validation/loginDatavalidation");
 const checkLogin = require("../middleware/common/checkLogin");
 const productAvatarUpload = require("../middleware/avatarUploader/productAvatar");
 const {
-  productValidation,
-  productValidationResult,
+    productValidation,
+    productValidationResult,
 } = require("../middleware/validation/productValidation");
 const {
-  checkRoleForUpdateProductImg,
+    checkRoleForUpdateProductImg,
 } = require("../middleware/common/checkRole");
+
+// Hello route
+routes.get("/", (req, res) => {
+    res.send("Hello world");
+});
 
 // GET USER ROUTE
 routes.get("/users/all", getUsers);
@@ -38,43 +44,44 @@ routes.get("/users/:role", getUsersByRole);
 
 // CREATE USER ROUTE
 routes.post(
-  "/user",
-  userAvatarUpload,
-  userValidation,
-  userValidationResult,
-  addUser
+    "/user",
+    userAvatarUpload,
+    userValidation,
+    userValidationResult,
+    addUser
 );
 
 // CREATE PRODUCT ROUTE
 routes.post(
-  "/product",
-  checkLogin,
-  productAvatarUpload,
-  productValidation,
-  productValidationResult,
-  addProduct
+    "/product",
+    checkLogin,
+    productAvatarUpload,
+    productValidation,
+    productValidationResult,
+    addProduct
 );
 
 // GET PRODUCTS ROUTE
 routes.get("/products/all", getProducts);
+routes.get("/product/:productId", getProduct);
 routes.get("/products/:status", getProductsByStatus);
 
 // UPDATE PRODUCT ROTE
 routes.put("/product/:id", checkLogin, updateProduct);
 routes.put(
-  "/product/image/:id",
-  checkLogin,
-  checkRoleForUpdateProductImg,
-  productAvatarUpload,
-  updateProductImg
+    "/product/image/:id",
+    checkLogin,
+    checkRoleForUpdateProductImg,
+    productAvatarUpload,
+    updateProductImg
 );
 
 // LOGIN ROUTE
 routes.post(
-  "/login",
-  loginUserDataValidation,
-  loginUserDataValidationResult,
-  loginController
+    "/login",
+    loginUserDataValidation,
+    loginUserDataValidationResult,
+    loginController
 );
 
 // CHECK LOGIN
